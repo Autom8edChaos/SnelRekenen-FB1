38fbccc75eb13b480d6f92f5e25e21a65e355343
John Doe <john@sample.com>
2016-12-20 16:26:39 +0100

    Added tests

---
+++ SnelRekenen.unittest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SnelRekenen.unittest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SnelRekenen.unittest")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("58e0dd83-28bb-4cab-89f3-4035ca7653cf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SnelRekenen.unittest/SumUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SnelRekenen;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SnelRekenen.unittest
+{
+    [TestClass]
+    public class SumUser
+    {
+        [TestMethod]
+        public void SumSomeNumbers()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void UnreliableOutcomes()
+        {
+            var seconds = DateTime.Now.Second;
+            Assert.AreEqual(0, (seconds % 2));
+        }
+
+    }
+}
